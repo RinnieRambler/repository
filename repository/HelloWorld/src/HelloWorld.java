7e4daf1f78c31f4e678c774d30e185178f6c1ec5
Corinne <87541484+RinnieRambler@users.noreply.github.com>
2024-02-29 21:46:39 -0400

    Add files via upload

---
+++ repository/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		
+		int num1 = 7;
+		int num2 = 8;
+		float myFloat1 = 2.83f;
+		float myFloat2 = 5.26f;
+		
+		System.out.println((num1 + num2) * (myFloat1 + myFloat2));
+	}
+
+}
